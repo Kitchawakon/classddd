f3cfc3d81e87418d438a32c6a69be9084d12db7a
Kitchawakon <119285236+Kitchawakon@users.noreply.github.com>
2022-11-28 15:41:21 +0700

    Add files via upload

---
+++ Laboop00A/Laboop00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ชื่อกฤตชวกร ชวลิตกิตติวงศ์
+Console.WriteLine("ชื่อ นาย กฤตชวกร ชวลิตกิตติวงศ์");
+// id
+Console.WriteLine("653450279-6");
+// e-mail
+Console.WriteLine("าitchawakon.c@kkumail.com");
